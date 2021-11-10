9b220705662fb9da3018968f9f97e30a8d7938a3
prajvalBolegave <prajval.21810470@viit.ac.in>
2021-11-10 11:54:28 +0530

    Basic templet not final front end

---
+++ client/src/components/Details/ReworkDetails.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+
+
+export default function ReworkDetails() {
+    return (
+        <div className="ReworkDetails">
+            <h1>rework report</h1>
+            <h1>rework report</h1>
+            <h1>rework report</h1>
+            <h1>rework report</h1>
+            <Button>hello</Button>
+
+            
+        </div>
+    )
+}
