e87dc0ecdde1c08ee10cef9ef284c1826714c656
prajvalBolegave <prajval.21810470@viit.ac.in>
2021-11-10 11:51:27 +0530

    frontend work

---
+++ client/src/components/Details/InspectionDetails.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ButtonGroup } from 'react-bootstrap'
+
+
+
+export default function InspectionDetails() {
+    return (
+        <div className="InspectionDetails">
+            <h1>inspection report</h1>
+            <h1>inspection report</h1>
+            <h1>inspection report</h1>
+            <h1>inspection report</h1>
+            <ButtonGroup></ButtonGroup>
+
+            
+        </div>
+    )
+}
---
+++ client/src/components/Details/ProductionDetails.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+
+
+export default function ProductionDetails() {
+    return (
+        <div className="ProductionDetails">
+            <h1>production report</h1>
+            <h1>production report</h1>
+            <h1>production report</h1>
+            <h1>production report</h1>
+            <Button>hello</Button>
+
+            
+        </div>
+    )
+}
