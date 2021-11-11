7f628f111c90a55e47df8aed49130cc1999a7064
prajvalBolegave <prajval.21810470@viit.ac.in>
2021-11-11 12:50:28 +0530

    user interface improvements more responsive layout and added navbar

--- client/src/components/Details/InspectionDetails.js
+++ client/src/components/Details/InspectionDetails.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Button, Container, Form,Row,Col,Nav} from 'react-bootstrap'
+import Navbar from '../Navbar/NavbarAdmin'
 
 
 
 export default function InspectionDetails() {
     return (
         <div className="InspectionDetails">
+            <Navbar/>
             <Container>
-            <Nav variant="tabs" defaultActiveKey="/inspection">
+                
+            <Nav variant="tabs" defaultActiveKey="/inspection" className="justify-content-md-center">
                 <Nav.Item>
                     <Nav.Link href="/inspection">Inspection Details</Nav.Link>
                 </Nav.Item>
@@ -20,47 +23,52 @@ export default function InspectionDetails() {
             </Nav>
         </Container>
         <br/>
-            <Container>
-                <Form>
-                    <Form.Group as={Row} className="mb-3">
-                            <Form.Label column sm ="3">Date of Inspection</Form.Label>
-                            <Col sm="4">
-                                <Form.Control type="date" placeholder="dd/mm/yy"></Form.Control>
-                            </Col>
-                    </Form.Group>
+            <Container >
+                    <Form>
+                        <Form.Group as={Row} className="justify-content-md-center">
+                                <Form.Label column sm ="3">Date of Inspection</Form.Label>
+                                <Col sm="4">
+                                    <Form.Control type="date" placeholder="dd/mm/yy"></Form.Control>
+                                </Col>
+                        </Form.Group>
+                        <br/>
 
-                    <Form.Group as={Row} className="mb-3">
-                            <Form.Label column sm ="3">Plant Code</Form.Label>
-                            <Col sm="4">
-                                <Form.Control></Form.Control>
-                            </Col>
-                    </Form.Group>
+                        <Form.Group as={Row} className="justify-content-md-center">
+                                <Form.Label column sm ="3">Plant Code</Form.Label>
+                                <Col sm="4">
+                                    <Form.Control></Form.Control>
+                                </Col>
+                        </Form.Group>
+                        <br/>
 
-                    <Form.Group as={Row} className="mb-3">
-                            <Form.Label column sm ="3">Production Line</Form.Label>
-                            <Col sm="4">
-                                <Form.Control ></Form.Control>
-                            </Col>
-                    </Form.Group>
+                        <Form.Group as={Row} className="justify-content-md-center">
+                                <Form.Label column sm ="3">Production Line</Form.Label>
+                                <Col sm="4">
+                                    <Form.Control ></Form.Control>
+                                </Col>
+                        </Form.Group>
+                        <br/>
 
-                    <Form.Group as={Row} className="mb-3">
-                            <Form.Label column sm ="3">Product no</Form.Label>
-                            <Col sm="4">
-                                <Form.Control ></Form.Control>
-                            </Col>
-                    </Form.Group>
+                        <Form.Group as={Row} className="justify-content-md-center">
+                                <Form.Label column sm ="3">Product no</Form.Label>
+                                <Col sm="4">
+                                    <Form.Control ></Form.Control>
+                                </Col>
+                        </Form.Group>
+                        <br/>
 
-                    <Form.Group as={Row} className="mb-5">
-                            <Form.Label column sm ="3">Product Name</Form.Label>
-                            <Col sm="4">
-                                <Form.Control ></Form.Control>
-                            </Col>
-                    </Form.Group>
+                        <Form.Group as={Row} className="justify-content-md-center">
+                                <Form.Label column sm ="3">Product Name</Form.Label>
+                                <Col sm="4">
+                                    <Form.Control ></Form.Control>
+                                </Col>
+                        </Form.Group>
+                        <br/>
 
-                    <Button variant="danger" onClick={event =>  window.location.href='/rework'}>Save and Next</Button>
+                        <Button variant="danger" onClick={event =>  window.location.href='/rework'}>Save and Next</Button>
 
 
-                </Form>
+                    </Form>
             </Container>
             
         </div>
--- client/src/components/Details/RejectionDetails.js
+++ client/src/components/Details/RejectionDetails.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Button,Container,Row,Col,Form,Nav } from 'react-bootstrap'
+import Navbar from '../Navbar/NavbarAdmin'
 
 
 
 export default function RejectionDetails() {
     return (
         <div className="RejectionDetails">
+            <Navbar/>
             <Container>
-            <Nav variant="tabs" defaultActiveKey="/rejection">
+            <Nav variant="tabs" defaultActiveKey="/rejection" className="justify-content-md-center">
                 <Nav.Item>
                     <Nav.Link href="/inspection">Inspection Details</Nav.Link>
                 </Nav.Item>
@@ -22,59 +24,67 @@ export default function RejectionDetails() {
             <Container>
                 <br/>
                 <Form>
-                <Form.Text as={Row} className="mb-3" >**Inprocess Rejection**</Form.Text>
-                    <Form.Group as={Row} className="mb-3">
+                <Form.Text as={Row} className="justify-content-md-center" >**Inprocess Rejection**</Form.Text>
+                <br/>
+                    <Form.Group as={Row} className="justify-content-md-center">
                             <Form.Label column sm ="3">Name of Process</Form.Label>
                             <Col sm="4">
                                 <Form.Control ></Form.Control>
                             </Col>
                     </Form.Group>
+                    <br/>
 
-                    <Form.Group as={Row} className="mb-3">
+                    <Form.Group as={Row} className="justify-content-md-center">
                             <Form.Label column sm ="3">No. of quantity:</Form.Label>
                             <Col sm="4">
                                 <Form.Control ></Form.Control>
                             </Col>
                     </Form.Group>
+                    <br/>
 
-                    <Form.Group as={Row} className="mb-3">
+                    <Form.Group as={Row} className="justify-content-md-center">
                             <Form.Label column sm ="3">No. of defective quantity:</Form.Label>
                             <Col sm="4">
                                 <Form.Control ></Form.Control>
                             </Col>
                     </Form.Group>
+                    <br/>
 
-                    <Form.Group as={Row} className="mb-3">
+                    <Form.Group as={Row} className="justify-content-md-center">
                             <Form.Label column sm ="3">Defect:</Form.Label>
                             <Col sm="4">
                                 <Form.Control ></Form.Control>
                             </Col>
                     </Form.Group>
+                    <br/>
 
-                    <Form.Group as={Row} className="mb-3">
+                    <Form.Group as={Row} className="justify-content-md-center">
                             <Form.Label column sm ="3">Location of Defect:</Form.Label>
                             <Col sm="4">
                                 <Form.Control ></Form.Control>
                             </Col>
                     </Form.Group>
+                    <br/>
 
-                    <Form.Group as={Row} className="mb-3">
+                    <Form.Group as={Row} className="justify-content-md-center">
                             <Form.Label column sm ="3">Category of Defect</Form.Label>
                             <Col sm="4">
                                 <Form.Control ></Form.Control>
                             </Col>
                     </Form.Group>
+                    <br/>
 
-                    <Form.Group as={Row} className="mb-3">
+                    <Form.Group as={Row} className="justify-content-md-center">
                             <Form.Label column sm ="3">Details</Form.Label>
                             <Col sm="4">
                                 <Form.Control ></Form.Control>
                             </Col>
                     </Form.Group>
+                    <br/>
 
-                    <Form.Group as={Row} className="mb-3">
+                    <Form.Group as={Row} className="justify-content-md-center">
                         <Form.Label column sm ="3">Rework</Form.Label>
-                        <Col>
+                        <Col sm="4">
                         {['radio'].map((type) => (
                             <div key={`inline-${type}`} className="mb-3">
                                 <Form.Check
@@ -95,14 +105,21 @@ export default function RejectionDetails() {
                         ))}
                         </Col>
                     </Form.Group>
+                    <br/>
 
-                    <Form.Group as={Row} className="mb-3">
+                    <Form.Group as={Row} className="justify-content-md-center">
                             <Form.Label column sm ="3">Details:</Form.Label>
                             <Col sm="4">
                                 <Form.Control as="textarea" rows={3}></Form.Control>
                             </Col>
                     </Form.Group>
-                    <Button variant="danger">Submit</Button>
+                    <br/>
+                    <Row className="justify-content-md-center">
+                        <Col sm="5"></Col>
+                        <Col sm="2">
+                            <Button variant="danger">Submit</Button>
+                        </Col>
+                    </Row>
                 </Form>
 
             </Container>
