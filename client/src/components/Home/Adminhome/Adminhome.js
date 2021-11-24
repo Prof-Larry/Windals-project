92c7bc761e4822f15c5520f9c7cf7d9969e716c5
Prof-Larry <ujjwalshirpurkar1@gmail.com>
2021-11-24 18:35:04 +0530

    recharts

--- client/src/components/Home/Adminhome/Adminhome.js
+++ client/src/components/Home/Adminhome/Adminhome.js
@@ -3,14 +3,57 @@ import { useEffect } from 'react';
 import "./Adminhome.css";
 import { useHistory } from 'react-router';
 import { Button, Container, Row, Col } from 'react-bootstrap';
-import Navbar from '../../Navbar/NavbarAdmin'
+import Navbar from '../../Navbar/NavbarAdmin';
+import { LineChart, Line, CartesianGrid, XAxis, Legend, YAxis, AreaChart, Area, Tooltip, ResponsiveContainer } from 'recharts';
 
 
 export default function Adminhome() {
 
     const [show, setShow] = useState(false);
     const [show1, setShow1] = useState(false);
-
+    const data = [{
+        name: 'Page A',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Page B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: 'Page C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: 'Page D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: 'Page E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: 'Page F',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: 'Page G',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    }
+    ];
 
     const history = useHistory();
     const checkAuthentication = async () => {
@@ -24,7 +67,7 @@ export default function Adminhome() {
             });
 
             const data = await response.json();
-            if (data.designation=="M") setShow1(true); 
+            if (data.designation == "M") setShow1(true);
             console.log(data);
             if (response.status !== 200) {
                 throw new Error(response.error);
@@ -42,7 +85,7 @@ export default function Adminhome() {
     }, []);
 
 
-    
+
     return (
         <div className="adminHome">
             <Navbar />
@@ -53,12 +96,54 @@ export default function Adminhome() {
                         <Row><Button variant="secondary" size="lg" className="mb-3" onClick={() => setShow(!show)}>PDI Report</Button></Row>
 
                         {show ? <Row><Button variant="outline-primary" size="lg" className="mb-3" onClick={event => window.location.href = '/searchbydate'}>View Report</Button></Row> : null}
-                        {show ? show1 ? <Row><Button variant="outline-primary" size="lg" className="mb-3" onClick={event => window.location.href = '/editreport'}>Edit Report</Button></Row> : null :null}
+                        {show ? show1 ? <Row><Button variant="outline-primary" size="lg" className="mb-3" onClick={event => window.location.href = '/editreport'}>Edit Report</Button></Row> : null : null}
                         {show ? <Row><Button variant="outline-primary" size="lg" className="mb-3" onClick={event => window.location.href = '/inspection'}>Submit Report</Button></Row> : null}
                         <Row><Button variant="secondary" size="lg" className="mb-3" onClick={event => window.location.href = '/myrework'}>My Rework</Button></Row>
 
                     </Col>
                 </Row>
+
+                <ResponsiveContainer aspect={4 / 2}>
+                    <LineChart
+                        width={500}
+                        height={300}
+                        data={data}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                    </LineChart>
+                </ResponsiveContainer>
+
+                <ResponsiveContainer aspect={4 / 2}>
+                    <AreaChart
+                        width={500}
+                        height={400}
+                        data={data}
+                        margin={{
+                            top: 10,
+                            right: 30,
+                            left: 0,
+                            bottom: 0,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+                    </AreaChart>
+                </ResponsiveContainer>
             </Container>
 
 
