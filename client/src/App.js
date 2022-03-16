b166c0df9bb5ea48b2fc12bddd29984a1f65ae4e
Prof-Larry <ujjwalshirpurkar1@gmail.com>
2022-03-16 16:25:53 +0530

    something

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import Register from "./components/Form/Adminlogin/Register";
 import AdminLogin from "./components/Form/Adminlogin/Adminlogin";
 import Home from "./components/Home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import Userlogin from "./components/Form/Userlogin/Userlogin";
 import Masterlogin from "./components/Form/Masterlogin/Masterlogin";
 import Adminhome from "./components/Home/Adminhome/Adminhome";
--- client/src/components/Details/MyRework.js
+++ client/src/components/Details/MyRework.js
@@ -96,7 +96,7 @@ export default function MyRework() {
                               size="sm"
                               onClick={handleClickComplete}
                             >
-                              Completed
+                              Complete
                             </Button>
                           </td>
                         </tr>
