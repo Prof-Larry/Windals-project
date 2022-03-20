7db6faf3ed4f922fd0c2d12e6a715951c2a51a77
Prof-Larry <ujjwalshirpurkar1@gmail.com>
2022-03-20 10:43:49 +0530

    testing hosting

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import Navbar from '../Navbar/Navbar'
+import axios from "axios";
+import React from "react";
+import Navbar from "../Navbar/Navbar";
 
 export default function Home() {
-    return (
-        <div>
-            <Navbar />
-        </div>
-    )
+  const handleTest = () => {
+    axios
+      .get("https://pdi-server.herokuapp.com/test")
+      .then((res) => {
+        alert(res.message);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div>
+      <Navbar />
+      <button onClick={handleTest}>Test</button>
+    </div>
+  );
 }
